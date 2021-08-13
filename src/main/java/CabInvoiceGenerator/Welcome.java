fd4dca01fa2a940220ec2161edcb065115cab58c
Akshithaaashi <akshitha.aashi@gmail.com>
2021-08-13 22:01:28 +0530

    Added : welcome message

---
+++ src/main/java/CabInvoiceGenerator/Welcome.java
@@ -0,0 +1,11 @@
+package CabInvoiceGenerator;
+
+public class Welcome {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Welcome Message
+		System.out.println("Program for the Cab Service");
+	}
+}
